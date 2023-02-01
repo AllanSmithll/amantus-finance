bc06f835eb194e327934e27a10390397d29c9432
allansmithll <allanammancio2004@gmail.com>
2023-01-31 22:48:43 -0300

    udpate

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  baseUrl: 'amantus-finance',
+  baseUrl: '/amantus-finance',
   plugins: [react()]
 })
