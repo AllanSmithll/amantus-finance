145ea20e27fc75ab3a2a9df4f0bcba170d6cf6fa
allansmithll <allanammancio2004@gmail.com>
2023-02-03 15:32:36 -0300

    Update: começando o primeiro Component

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../css/Cabecalho.css'
+
+class Cabecalho extends Component {
+    render() {
+        return(
+            <div>
+                <header>
+                    <section id="burguer">
+                        
+                    </section>
+                    <section>
+                        <h1>Amantus Finance</h1>
+                    </section>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Cabecalho;
